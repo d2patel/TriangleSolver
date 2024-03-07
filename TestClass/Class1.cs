34a6af30d6c52dfa55a119cbbbf86d72312b287a
D2's laptop <D2's laptop@D2LAPTOP>
2024-03-06 22:44:02 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace TestClass
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void AnalyzeTriangle_Equilateral_Test1()
+        {
+            int firstSide = 10;
+            int secondSide = 10;
+            int thirdSide = 10;
+
+            string actual = "An equilateral triangle is formed";
+
+            string expexted = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);   
+
+            Assert.That(actual,Is.EqualTo(expexted));
+        }
+    }
+}
